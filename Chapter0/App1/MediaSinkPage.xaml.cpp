368cf5d6a3e249f956bb571a4fddb180d7e54641
luncliff <luncliff@gmail.com>
2022-09-27 03:26:24 +0900

    get required COMs in MedisSinkPage

--- Chapter0/App1/MediaSinkPage.xaml.cpp
+++ Chapter0/App1/MediaSinkPage.xaml.cpp
@@ -1,4 +1,7 @@
 ﻿#include "pch.h"
+
+#include <winrt/Windows.UI.Core.h>
+
 #include "MediaSinkPage.xaml.h"
 #if __has_include("MediaSinkPage.g.cpp")
 #include "MediaSinkPage.g.cpp"
@@ -6,14 +9,51 @@
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
+using winrt::Windows::UI::Core::CoreWindow;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace winrt::App1::implementation {
 
-namespace winrt::App1::implementation
-{
-    MediaSinkPage::MediaSinkPage()
-    {
-        InitializeComponent();
+MediaSinkPage::MediaSinkPage() {
+    InitializeComponent();
+    D3D_FEATURE_LEVEL device_feature_level = D3D_FEATURE_LEVEL_11_0;
+    D3D_FEATURE_LEVEL levels[]{D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL_11_0};
+    if (auto hr = D3D11CreateDevice(
+            nullptr, D3D_DRIVER_TYPE_HARDWARE, NULL,
+            D3D11_CREATE_DEVICE_SINGLETHREADED | D3D11_CREATE_DEVICE_BGRA_SUPPORT | D3D11_CREATE_DEVICE_VIDEO_SUPPORT,
+            levels, 2, D3D11_SDK_VERSION, device.put(), &device_feature_level, device_context.put());
+        FAILED(hr)) {
+        spdlog::error("{}: {}", "MediaSinkPage", winrt::to_string(winrt::hresult_error{hr}.message()));
+        winrt::throw_hresult(hr);
+    }
+    winrt::com_ptr<IDXGIDevice2> dxgi_device{};
+    winrt::check_hresult(device->QueryInterface(dxgi_device.put()));
+    winrt::com_ptr<IDXGIAdapter> adapter{};
+    winrt::check_hresult(dxgi_device->GetParent(__uuidof(IDXGIAdapter), adapter.put_void()));
+    winrt::check_hresult(adapter->GetParent(__uuidof(IDXGIFactory2), factory.put_void()));
+
+    if (device_feature_level >= D3D_FEATURE_LEVEL_11_0) {
+        IUnknown* unknown = winrt::get_unknown(swapchainPanel());
+        // unknown.as(__uuidof(ISwapChainPanelNative), bridge.put_void());
+        if (auto hr = unknown->QueryInterface(bridge.put()); FAILED(hr))
+            winrt::throw_hresult(hr);
     }
 }
+
+void MediaSinkPage::use_window(winrt::Windows::UI::Core::CoreWindow window) {
+    DXGI_SWAP_CHAIN_DESC1 desc{};
+    winrt::check_hresult(factory->CreateSwapChainForCoreWindow( //
+        device.get(), winrt::get_unknown(window), &desc, nullptr, swapchain.put()));
+    if (swapchain)
+        bridge->SetSwapChain(swapchain.get());
+}
+
+void MediaSinkPage::use_window(HWND handle) {
+    DXGI_SWAP_CHAIN_DESC1 desc{};
+    DXGI_SWAP_CHAIN_FULLSCREEN_DESC fullscreen{};
+    winrt::check_hresult(factory->CreateSwapChainForHwnd( //
+        device.get(), handle, &desc, &fullscreen, nullptr, swapchain.put()));
+    if (swapchain)
+        bridge->SetSwapChain(swapchain.get());
+}
+
+} // namespace winrt::App1::implementation
--- Chapter0/App1/MediaSinkPage.xaml.h
+++ Chapter0/App1/MediaSinkPage.xaml.h
@@ -2,11 +2,36 @@
 
 #include "MediaSinkPage.g.h"
 
+#include <d3d11_4.h>
+#include <dxgi1_6.h>
+#include <evr.h>
+#include <mfapi.h>
+#include <mferror.h>
+#include <mfidl.h>
+
+#include <microsoft.ui.xaml.media.dxinterop.h> // NOT <windows.ui.xaml.media.dxinterop.h>
+#include <windows.graphics.capture.interop.h>
+#include <windows.graphics.directx.direct3d11.h>
+#include <windows.graphics.directx.direct3d11.interop.h>
+
 namespace winrt::App1::implementation {
 
+/// @see https://learn.microsoft.com/en-us/windows/uwp/composition/visual-layer
+/// @see https://learn.microsoft.com/en-us/uwp/api/windows.ui.xaml.controls.swapchainpanel
 /// @see https://github.com/KlearTouch/KlearTouch.MediaPlayer
 struct MediaSinkPage : MediaSinkPageT<MediaSinkPage> {
     MediaSinkPage();
+
+    /// @todo export with IDL
+    void use_window(winrt::Windows::UI::Core::CoreWindow);
+    void use_window(HWND handle);
+
+  private:
+    winrt::com_ptr<ID3D11Device> device = nullptr;
+    winrt::com_ptr<ID3D11DeviceContext> device_context = nullptr;
+    winrt::com_ptr<IDXGIFactory2> factory = nullptr;
+    winrt::com_ptr<ISwapChainPanelNative> bridge = nullptr;
+    winrt::com_ptr<IDXGISwapChain1> swapchain = nullptr;
 };
 
 } // namespace winrt::App1::implementation
